0a888878b6850c189c83567c046f1b75b1dc6670
Ismayel <ismayelhossen123@gmail.com>
2020-07-21 22:50:55 +0600

    Add Notification functionality to send mail to user .now here present two functionality first is Mail and second is Notifications to send mail

--- Mail/MailController.php
+++ Mail/MailController.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use App\Mail\WelcomeMail;
+use App\Notifications\Welcomenotif;
 use Mail;
+use DB;
+use App\User;
 class MailController extends Controller
 {
     public function index(){
@@ -16,10 +18,15 @@ class MailController extends Controller
           $data=array();
           $email=$request->email;
             $password=$request->password;
+            //first option using Mail
    $mail= Mail::to($request->email)->send(new WelcomeMail($email,$password));
-     if($mail){
-         return Redirect()->back();
-     }
+//      if($mail){
+//          return Redirect()->back();
+//      }
+//second option using Notifications
+$user=User::find(1);
+ $user->notify(new Welcomenotif($user));
+
 
 
     }
