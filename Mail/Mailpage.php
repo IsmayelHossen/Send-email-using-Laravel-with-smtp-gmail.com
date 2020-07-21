5370f6e956a77e8664559daf01646fec83bc76c7
Ismayel <ismayelhossen123@gmail.com>
2020-07-21 15:09:20 +0600

    Create Mailpage which is actully see user from their mail and put here requirement functionality

---
+++ Mail/Mailpage.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>
+    {{$email}}
+        {{$password}}
+    </h2>
+    <h4 style="color:red">Bangladeah is the developing country in the world</h4>
+</body>
+</html>
